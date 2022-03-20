4334f1e3e6f9018003be8130adb5f12f1a5e699f
arielMsi <ariel.taipe2640@utc.edu.ec>
2022-03-19 23:43:31 -0500

    agrgando a eli

--- application/views/encabezado.php
+++ application/views/encabezado.php
@@ -106,7 +106,7 @@
   </div>
   <div class="col-md-11 text-left">
     <h2>
-      <b>GESTION DE AULAS Y BLOQUES ARIEL ISAIAS</b>
+      <b>GESTION DE AULAS Y BLOQUES ARIEL ISAIAS y ELI</b>
     </h2>
 
   </div>
