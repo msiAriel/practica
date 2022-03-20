c9c8d7bbbc2e9a008597049cd19991e8304d3526
msiAriel <101957175+msiAriel@users.noreply.github.com>
2022-03-19 22:56:06 -0500

    nuevo encabezado

--- application/views/encabezado.php
+++ application/views/encabezado.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <title>Practicando para el GIT</title>
+    <title>Practicando para el GIt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://code.jquery.com/jquery-3.6.0.js"integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
